70582ed113c6be42b4a572db5aca07b3ffc3af00
zeesuulan <zeesuulan@gmail.com>
2014-08-31 18:34:26 +0800

    deltel

--- wap/js/controller/intro.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-angular.module("JT")
-	.controller("c_intro", 
-		function($scope, $rootScope) {
-			$rootScope.pageTitle = PAGE_TITLE + "-- 查看活动"
-			$rootScope.backURL = "/#/menu"
-			$rootScope.showNav = true
-		}
-	);
